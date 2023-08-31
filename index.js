39e1db03f15b3d6d20cbe458f6a26657e4d38ec8
NathaliRodrigues <142436453+NathaliRodrigues@users.noreply.github.com>
2023-08-31 17:03:10 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+//Nathali Rodrigues
+//exercicio 1
+/*
+10
+50
+não apareceria nada
+ 
+// exercicio 2
+ele vai transforma o texto do usuario em minúsculo e procura o caracter "cenoura"
+true
+true
+false
+
+
+//exercicio escrita de codigo B a
+function imprimirminhasinfo()
+{console.log("Eu sou Nathali, tenho 18 anos, moro em são leopoldo e sou estudante")};
+imprimirminhasinfo()
+
+// B b
+function minhasinformaçoes(nome,idade,cidade,profissao){
+nome = nome.toString();
+idade = Number(idade);
+cidade = cidade.toString();
+profissao = profissao.toString();
+return ("eu sou " +  nome  + " tenho "  + idade +  " anos " + " moro em " + cidade + " sou " + profissao);
+}
+console.log(minhasinformaçoes ("Nathali" , 18, "sao leo" , "desempregada " ));
+
+//exercicio B 2a
+function calculo(n1,n2){
+return n1 + n2;
+}
+console.log("result", + calculo(5,5));
+
+//exercicio B 2b
+
+function teste(n1,n2) {
+return (n1 >= n2);
+}
+console.log(teste(4,7));
+
+
+//B 2c
+function numero(b);{
+retur (b % 2 === 0);
+}
+
+//B 2d
+function tamanhoEMaiusculas(a){
+    return a.length + " " + a.toUpperCase();
+}
+let frase1 = prompt("Digite uma frase?");
+console.log(tamanhoEMaiusculas(frase1));
+
+//exercicio 3
+
+function soma(a,b){
+    return a+b;
+}
+
+function divisao(a,b){
+return a/b;
+}
+function subtracao(a,b){
+return a-b;
+}
+function multiplicacao(a,b){
+return a*b;
+}
+
+let n = Number (prompt("digite um numero?"));
+let n1 = Number (prompt("digite outro numero?"));
+console.log(soma(n, n1));
+console.log(divisao(n,n1));
+console.log(subtracao(n,n1));
+console.log(multiplicacao(n,n1));
+*/
